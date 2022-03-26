060f0f8368efa666c586bc34546fcc06945091a4
gbh10c <grandcasa10@gmail.com>
2022-03-25 20:49:37 -0400

    Added basic review features

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const expressError = require('./utilities/expressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const ExpressError = require('./utilities/expressError');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {});
 
@@ -77,6 +78,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 app.all('*', (req, res, next) => {
