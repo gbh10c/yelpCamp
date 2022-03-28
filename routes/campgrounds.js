5a097d24c146bb54eaca1029384e181eb0f04e5c
gbh10c <grandcasa10@gmail.com>
2022-03-28 11:52:56 -0400

    Set up folder for static assets, configured session, added flash alerts for success and errors

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,7 +18,7 @@ const validateCampground = (req, res, next) => {
 
 router.get('/', catchAsync(async (req, res) => {
   const campgrounds = await Campground.find({});
-  res.render('campgrounds/index', { campgrounds })
+  res.render('campgrounds/index', { campgrounds });
 }));
 
 router.get('/new', (req, res) => {
@@ -27,31 +27,41 @@ router.get('/new', (req, res) => {
 
 router.post('/', validateCampground, catchAsync(async (req, res, next) => {
   // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-
   const campground = new Campground(req.body.campground);
   await campground.save();
+  req.flash('success', 'Successfully created a new campground!');
   res.redirect(`/campgrounds/${campground._id}`)
 }));
 
 router.get('/:id', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id).populate('reviews');
+  if(!campground){
+    req.flash('error', 'Cannot find that campground!');
+    return res.redirect('/campgrounds');
+  }
   res.render('campgrounds/show', { campground });
 }));
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
+  if(!campground){
+    req.flash('error', 'Cannot find that campground!');
+    return res.redirect('/campgrounds');
+  }
   res.render('campgrounds/edit', { campground });
 }));
 
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  req.flash('success', 'Successfully updated campground!')
   res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.delete('/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
+  req.flash('success', 'Deleted campground!')
   res.redirect('/campgrounds');
 }))
 
