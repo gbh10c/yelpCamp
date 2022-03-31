e9b0d41ed77d2bd032c0f2537309a4f7ede7c2cf
gbh10c <grandcasa10@gmail.com>
2022-03-31 00:35:00 -0400

    Authorization throughout app

--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async() => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: '6243d896fe13238c6db5fe7a',
       location:`${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: 'http://source.unsplash.com/collection/483251',
