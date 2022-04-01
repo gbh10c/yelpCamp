d9d9b96674037ff14cbcf6b34e4b299be405078b
gbh10c <grandcasa10@gmail.com>
2022-04-01 18:02:47 -0400

    Added photos to campground form, used multer, cloudinary, and multer-storage-cloudinary

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -11,15 +11,17 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCampground = async (req, res, next) => {
   const campground = new Campground(req.body.campground);
+  campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
   campground.author = req.user._id;
   await campground.save();
+  console.log(campground);
   req.flash('success', 'Successfully created a new campground!');
   res.redirect(`/campgrounds/${campground._id}`)
 }
 
 module.exports.showCampground = async (req, res) => {
   const campground = await Campground.findById(req.params.id).populate({
-    path:'reviews',
+    path: 'reviews',
     populate: {
       path: 'author'
     }
