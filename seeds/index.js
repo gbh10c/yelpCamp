620bebdc86ac30b99c8388fcc489a962c1436f62
gbh10c <grandcasa10@gmail.com>
2022-04-02 18:31:54 -0400

    Added image field to edit page. Also identified images to delete on edit page

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,23 @@ const seedDB = async() => {
       author: '6243d896fe13238c6db5fe7a',
       location:`${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'http://source.unsplash.com/collection/483251',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eligendi maiores repellendus laboriosam aspernatur nostrum temporibus, vero necessitatibus, laudantium esse eaque neque quas porro, distinctio illum? Id aperiam doloribus eaque?',
       price,
+      images: [
+        {
+          url: 'https://res.cloudinary.com/gbh-cloud/image/upload/v1648856266/YelpCamp/xpjymyrwph8rviql1xsf.jpg',
+          filename: 'YelpCamp/xpjymyrwph8rviql1xsf',
+        },
+        {
+          url: 'https://res.cloudinary.com/gbh-cloud/image/upload/v1648856267/YelpCamp/zbjk9svk2ueoytjoe5bi.jpg',
+          filename: 'YelpCamp/zbjk9svk2ueoytjoe5bi',
+        },
+        {
+          url: 'https://res.cloudinary.com/gbh-cloud/image/upload/v1648856267/YelpCamp/vv5jpq5y143gocyhuw7t.jpg',
+          filename: 'YelpCamp/vv5jpq5y143gocyhuw7t',
+        }
+      ],
+    
     })
     await camp.save();
   }
